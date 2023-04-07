615708a289ba2c42228bbb02af4ad8f9663aaf18
Tim von Asen <timvonasen@users.noreply.github.com>
2023-04-07 18:37:44 +0200

    SetUp resent Game
    My old Game as a template to get things done.

--- saftbrunnen/src/App.js
+++ saftbrunnen/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import MainMenu from "./Components/MainMenu";
+import Quiz from "./Components/Quiz";
+import ScoreBoard from "./Components/ScoreBoard";
+import { QuizContext } from "./Helpers/Contexts";
 
 function App() {
+  const [gameState, setGameState] = useState("menu");
+  const [userName, setUserName] = useState("menu");
+  const [score, setScore] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* Will be displayed above every GameState */}
+      <header className="Header">
+        <h1>MARVEL</h1>
       </header>
+      <div className="SmallHeader">
+        <h3>Characters Quiz</h3>
+      </div>
+
+      <QuizContext.Provider
+        value={{
+          gameState,
+          setGameState,
+          userName,
+          setUserName,
+          score,
+          setScore,
+        }}
+      >
+        {gameState === "menu" && <MainMenu />}
+        {gameState === "quiz" && <Quiz />}
+        {gameState === "scoreBoard" && <ScoreBoard />}
+      </QuizContext.Provider>
+      <footer className="Footer">
+        <h3>
+          <a href="https://github.com/timvonasen">© 2023 Tim von Asen</a>
+        </h3>
+      </footer>
     </div>
   );
 }
